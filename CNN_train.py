e650ef66127d8e77249f1f356326d9d0aab87998
Sarighrayeb <ghrayeb.sari@gmail.com>
2021-11-14 16:54:34 +0200

    sari

--- CNN_train.py
+++ CNN_train.py
@@ -15,9 +15,17 @@ from tensorflow.keras import layers
 from tensorflow.keras import optimizers
 from tensorflow.keras import callbacks
 from tensorflow.keras import regularizers
+lr = 0
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
+COLOR_CHANNELS = 3
 from tensorflow.keras import models
 
 
 def build_model_gender(learning_rate=lr):
     model = models.Sequential()
-    conv_base=tf.keras.applications.
+    conv_base = tf.keras.applications.resnet.ResNet152(include_top=False, weights='imagenet',
+                                                     input_shape=(IMG_WIDTH, IMG_HEIGHT, COLOR_CHANNELS), classes=2)
+    conv_base.trainable = False
+    model.add(conv_base)
+    print(model.summary())
